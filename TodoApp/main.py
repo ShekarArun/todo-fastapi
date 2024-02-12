ed1276d8efe2012f014d44c334bbbbb11a6083dc
Arun Shekar <shekar.arun@gmail.com>
2024-02-12 15:33:59 +0530

    feat: Add endpoint to create TODO

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 import models
@@ -22,6 +23,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get('/')
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -33,3 +41,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model:
         return todo_model
     raise HTTPException(status_code=404, detail='Specific task not found')
+
+
+@app.post('/todo', status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
